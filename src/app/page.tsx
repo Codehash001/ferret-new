1065a495999525697fb4224c6634bd8cec64993e
lakmalmabm <lakmalmabm@gmail.com>
2024-02-06 12:22:28 +0530

    error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       </div>
       <div className="flex flex-1 py-4">
         <div className="w-full">
-          <Chat />
+          {/* <Chat /> */}
         </div>
       </div>
     </main>
